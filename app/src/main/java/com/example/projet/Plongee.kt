fa0376a2accabdd65986ea26e8561a1801a7dab7
Léo <22008676@etu.unicaen.fr>
2023-03-11 19:53:20 +0100

    tqt

--- app/src/main/java/com/example/projet/Plongee.kt
+++ app/src/main/java/com/example/projet/Plongee.kt
@@ -3,28 +3,30 @@ package com.example.projet
 import android.content.Context
 import android.os.Build
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.projet.bdd.BDD
 import com.example.projet.bdd.DatabaseFiller
 import com.example.projet.bdd.viewModel.*
 import com.example.projet.ui.theme.ProjetTheme
 
 class Plongee : ComponentActivity() {
-    companion object{
+    companion object {
         private var context: Context? = null
 
         fun getAppContext(): Context? {
@@ -38,9 +40,9 @@ class Plongee : ComponentActivity() {
 
         super.onCreate(savedInstanceState)
 
-        Thread{
+        Thread {
             // fill database if it is empty
-            if(BDD.getInstance(Plongee.getAppContext()!!).SkillDao().getNumberOfSkills()==0){
+            if (BDD.getInstance(Plongee.getAppContext()!!).SkillDao().getNumberOfSkills() == 0) {
                 Plongee.getAppContext()!!.deleteDatabase("BDD_plongeurs")
                 BDD.getInstance(Plongee.getAppContext()!!)
 
@@ -65,7 +67,7 @@ class Plongee : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     Column {
-                        tableauFormation()
+                        tableauFormation(aptitudeVM)
                     }
                 }
             }
@@ -74,13 +76,36 @@ class Plongee : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Greeting(name: String, aptitudeVM: AptitudeViewModel) {
+    tableauFormation(aptitudeVM)
 }
 
 @Composable
-fun tableauFormation(){
+fun tableauFormation(aptitudeVM: AptitudeViewModel) {
+    val aptitudeName = remember {
+        mutableStateOf("")
+    }
+    var liste = apti.getAll()
 
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp)
+    ) {
+        Text(text = "tesjfhzerbfekuuhbfzkb")
+        Text(text = aptitudeName.value, modifier = Modifier.fillMaxWidth())
+
+        if (liste != null) {
+            liste.forEach{ chanson ->
+                Row(modifier = Modifier.fillMaxWidth()) {
+                    Text(text = chanson.auteur)
+                }
+            }
+        }
+        else {
+            Text(text = "Liste Vide",modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center)
+        }
+    }
 }
 
 @Preview(showBackground = true)
